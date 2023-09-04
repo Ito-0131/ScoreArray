4ed04b069474afa29f96aa7504e3c135f425fb82
Ito-0131 <berufegoal@yahoo.co.jp>
2023-09-04 20:54:03 +0900

    配列に関するサンプルプログラム

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+class ScoreArray {
+    public static void main(String[] args) {
+        int[] date = new int[4];
+
+        date[0] = 90;
+        date[1] = 80;
+        date[2] = 100;
+        date[3] = 90;
+
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
