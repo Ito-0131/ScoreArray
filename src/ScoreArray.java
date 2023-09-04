07600d6bf644a666bf83177d51c71fce794283bf
Ito-0131 <berufegoal@yahoo.co.jp>
2023-09-04 21:23:24 +0900

    ・Main.javaからScoreArray.javaにファイル名を変更。 ・10～13行目を修正

--- src/ScoreArray.java
+++ src/ScoreArray.java
@@ -7,9 +7,9 @@ class ScoreArray {
         date[2] = 100;
         date[3] = 90;
 
-        System.out.println(0);
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(date[0]);
+        System.out.println(date[1]);
+        System.out.println(date[2]);
+        System.out.println(date[3]);
     }
 }
